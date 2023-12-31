4a7311350bb51b20146378b8fba114e0fe030c90
Kaytheman99 <88041769+Kaytheman99@users.noreply.github.com>
2023-12-31 10:30:34 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,7 @@
+function calculateNumbers(var1, var2) {
+    const sum = var1 + var2;
+    const diff = var1 - var2;
+    const mult = var1 * var2;
+    const div = var1 / var2;
+    return [sum, diff, mult, div];
+}
